7dcc5a1c93230c9c31e232c0b257741f2d80da6d
erlm <erlm@kea.dk>
2025-02-12 11:48:43 +0100

    nu med objects

--- js/dropdown2.js
+++ js/dropdown2.js
@@ -9,12 +9,31 @@ function addColors2(btn) {
     console.log(document.querySelectorAll("*"));
 }
 
+function addColorsObject(btn) {
+    ddColor2.innerHTML = ""
+    colorObjects.forEach(fillDropdownObjects);
+    //console.log(document.querySelectorAll("*"))
+}
+
 function fillDropdown2(item) {
     const el = document.createElement("option");
     el.textContent = item[0];
     el.value = item[1];
     ddColor2.appendChild(el);
 }
+
+function fillDropdownObjects(item) {
+    const el = document.createElement("option")
+    el.textContent = item.name
+    el.value = item
+    el.colorItem = item
+    console.log(item)
+    console.log(el.value)
+    console.log(item.toString())
+    el.colorItem = item
+    ddColor2.appendChild(el);
+}
+
 
 function setBackgroundColor2(ev) {
     console.log(ev);
@@ -24,9 +43,20 @@ function setBackgroundColor2(ev) {
     bdy.style.backgroundColor = color;
 }
 
+function setBackgroundColorObjects(ev) {
+    console.log(ev)
+    const selindex = ddColor2.selectedIndex;
+    const selectedOption = ddColor2.options[selindex];
+    //const color = selectedOption.value
+    console.log(selectedOption)
+    const color = selectedOption.colorItem.id
+    console.log(color)
+    bdy.style.backgroundColor = color
+}
+
 const pbFillDropdown2 = document.getElementById("pbFillDropdown2");
 const ddColor2 = document.getElementById("ddColor2");
 
-pbFillDropdown2.addEventListener("click", addColors2);
-ddColor2.addEventListener("change", setBackgroundColor2);
+pbFillDropdown2.addEventListener("click", addColorsObject);
+ddColor2.addEventListener("change", setBackgroundColorObjects);
 
